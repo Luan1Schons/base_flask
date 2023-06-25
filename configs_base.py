2a889ef86bdda6435ebe37f14977841d45554cb6
Luan Schons <luanschons2000@gmail.com>
2023-06-24 23:09:07 -0300

    Alterações

--- configs_base.py
+++ configs_base.py
@@ -3,14 +3,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    JWT_SECRET_KEY = "yjKrA841DAjuP84XmnyTz_uNGz7TOgsYZcg"
+    JWT_SECRET_KEY = "SECRET_KEY"
     # Configurações gerais
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua-chave-secreta-aqui'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Configurações do banco de dados MySQL
     DB_USERNAME = 'root'
-    DB_PASSWORD = '123321lek'
+    DB_PASSWORD = ''
     DB_HOST = 'localhost'
     DB_PORT = '3306'
     DB_NAME = 'app'
