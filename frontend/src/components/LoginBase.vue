1fbd0a77b05ca2d78120c9b234258cdb51279868
Luan Schons <luanschons2000@gmail.com>
2023-06-25 09:01:14 -0300

    Configuração Front-End utilizando vue.js

---
+++ frontend/src/components/LoginBase.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="main">
+  <div class="container h-100 w-100">
+    <div class="row justify-content-center align-items-center h-100">
+      <div class="col-md-6">
+        <Card title="Autenticação">
+          <template #content>
+            <form @submit.prevent="handleForm">
+              <div class="form-group mb-3">
+                <label for="email">Email:</label>
+                <InputField label="Email:" :value="email" @update:value="email = $event" type="text" class="form-control" required />
+              </div>
+              <div class="form-group">
+                <label for="password">Senha:</label>
+                <InputField
+                  label="Senha:"
+                  @update:value="password = $event"
+                  v-model="password"
+                  type="password"
+                  class="form-control"
+                  required
+                />
+              </div>
+              <div class="row justify-content-end align-items-center w-100">
+                <Button type="success" class="mt-3">Enviar</Button>
+              </div>
+            </form>
+          </template>
+        </Card>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+  
+  <script>
+  import InputField from './inputs/InputField.vue'
+  import Button from './buttons/Button.vue'
+  import Card from './Card.vue'
+  
+  export default {
+  components: {
+    InputField,
+    Button,
+    Card,
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    handleForm() {
+      console.log('Email:', this.email);
+      console.log('Password:', this.password);
+
+      const postData = {
+        email: this.email,
+        password: this.password,
+      };
+
+      this.$axios
+        .post('/auth', postData)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+  },
+};
+  </script>
+  
+  <style>
+
+  </style>
+  
