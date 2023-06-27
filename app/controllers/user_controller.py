a29ed169828f4fd2624ee09723b2479532534807
Luan Schons <luanschons2000@gmail.com>
2023-06-27 08:05:11 -0300

    Alterações

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,8 +1,8 @@
-from app.models.user import User
-from app import db
-from app.helpers.utils import Utils
 from sqlalchemy.exc import SQLAlchemyError
 from flask_restful import marshal, fields
+from app import db
+from app.models.user import User
+from app.helpers.utils import Utils
 
 def create_user(username, email, password):
     user = User(username=username, email=email, password=password)
@@ -16,27 +16,31 @@ def create_user(username, email, password):
         error_message = Utils.get_error_message(e)
         return False, error_message
 
+
 def get_user(user_id):
     user = User.query.get(user_id)
     if user:
         user_data = {
-            'id': user.id,
-            'username': user.username,
-            'email': user.email,
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
         }
         return user_data
     else:
         return None
 
+
 def update_user(user_id, username, email):
     user = User.query.get(user_id)
 
     if user is None:
-        return False, 'Usuário não encontrado.'
+        return False, "Usuário não encontrado."
 
     # Verificar se outro usuário já possui o mesmo username ou email
-    if User.query.filter(User.id != user_id, (User.username == username) | (User.email == email)).first():
-        return False, 'Porfavor escolha um username ou email diferente.'
+    if User.query.filter(
+        User.id != user_id, (User.username == username) | (User.email == email)
+    ).first():
+        return False, "Porfavor escolha um username ou email diferente."
 
     # Atualizar os dados do usuário
     user.username = username
@@ -50,11 +54,12 @@ def update_user(user_id, username, email):
         error_message = Utils.get_error_message(e)
         return False, error_message
 
+
 def delete_user(user_id):
     user = User.query.get(user_id)
 
     if user is None:
-        return False, 'Usuário não encontrado.'
+        return False, "Usuário não encontrado."
 
     try:
         db.session.delete(user)
@@ -64,23 +69,27 @@ def delete_user(user_id):
         db.session.rollback()
         error_message = Utils.get_error_message(e)
         return False, error_message
-    
+
+
 def users_list(page, per_page, sort_by, sort_order):
     try:
         # Defina a ordenação com base nos parâmetros recebidos
-        sort_column = getattr(User, sort_by) if hasattr(User, sort_by) else User.username
-        sort_direction = getattr(sort_column, sort_order.lower(), 'asc')
+        sort_column = (
+            getattr(User, sort_by) if hasattr(User, sort_by) else User.username
+        )
+        sort_direction = getattr(sort_column, sort_order.lower(), "asc")
 
-        users_query = User.query.order_by(sort_direction()).paginate(page=page, per_page=per_page)
+        users_query = User.query.order_by(sort_direction()).paginate(
+            page=page, per_page=per_page
+        )
         total_users = users_query.total
         users = users_query.items
 
         if users:
-
             user_fields = {
-                'id': fields.Integer,
-                'username': fields.String,
-                'email': fields.String
+                "id": fields.Integer,
+                "username": fields.String,
+                "email": fields.String,
             }
 
             serialized_users = [marshal(user, user_fields) for user in users]
@@ -89,4 +98,3 @@ def users_list(page, per_page, sort_by, sort_order):
     except Exception as e:
         error_message = Utils.get_error_message(e)
         return None, 0, error_message
-    
